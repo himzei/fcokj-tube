f679d7b2d55f214ef4976807c2fddcc57b01b60c
himzei <himzei@gmail.com>
2021-04-15 20:41:35 +0900

    Making Router

---
+++ README.md
@@ -0,0 +1,21 @@
+# fcokj tube
+
+/
+
+/users/join
+/users/login
+/users/edit
+/users/delete
+
+/videos/search
+/videos/watch
+/videos/edit
+/videos/delete
+/videos/comments
+
+/boards/search
+/boards/view
+/boards/edit
+/boards/edit
+/boards/delete
+/boards/comments
--- src/server.js
+++ src/server.js
@@ -5,19 +5,36 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  console.log("I will respond.");
-  return res.send("hellO");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+const boardRouter = express.Router();
+
+const handleViewBoard = (req, res) => res.send("View Board");
+
+boardRouter.get("/view", handleViewBoard);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/boards", boardRouter);
 
 const handleListening = () => console.log(`✅http://localhost:${PORT}`);
 
