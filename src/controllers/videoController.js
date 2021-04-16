77e4a7484727c8096352a1834c897fe1bf5a75b0
himzei <himzei@gmail.com>
2021-04-16 18:07:34 +0900

    Creating a Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,31 +1,7 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Thrid Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
+export const home = async (req, res) => {
+  const videos = await Video.find({});
   res.render("home", { pageTitle: "홈", videos });
 };
 
@@ -33,28 +9,23 @@ export const watch = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
 };
 
 export const getEdit = (req, res) => {
   const {
     params: { id },
   } = req;
-  const video = videos[id - 1];
 
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing: ${video.title}` });
 };
 
 export const postEdit = (req, res) => {
   const {
     params: { id },
   } = req;
-  const {
-    body: { title },
-  } = req;
-  const video = videos[id - 1];
-  video.title = title;
+
   return res.redirect(`/videos/${id}`);
 };
 
@@ -62,19 +33,25 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "upload video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { title },
+    body: { title, description, hashtags },
   } = req;
-  const newVideo = {
+
+  await Video.create({
     title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags
+      .split(",")
+      .strip(" ")
+      .map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+
   return res.redirect("/");
 };
 
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import videoRouter from "../routers/videoRouter";
 
 const videoSchema = new mongoose.Schema({
   title: String,
