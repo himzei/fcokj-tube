f02bbef5532df99528327d3ef8d83dd437741d57
himzei <himzei@gmail.com>
2021-04-17 08:45:18 +0900

    Creage User Model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,10 @@
-export const join = (req, res) => res.send("join");
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Create Account" });
+};
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  res.end();
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
