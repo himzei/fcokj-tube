2af5df42a5ccc3f8af9debbc60fdef70b4845e0e
himzei <himzei@gmail.com>
2021-04-16 11:12:33 +0900

    Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,6 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
-  console.log(req.params);
-  res.send("watch");
+  return res.render("watch");
 };
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
