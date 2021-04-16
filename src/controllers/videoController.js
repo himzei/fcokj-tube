6002582920f0f75d265786e6ec09e22228ad1e54
himzei <himzei@gmail.com>
2021-04-16 15:29:34 +0900

    create video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -58,6 +58,25 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "upload video" });
+};
+
+export const postUpload = (req, res) => {
+  const {
+    body: { title },
+  } = req;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
+
 export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,15 +4,16 @@ import {
   getEdit,
   postEdit,
   watch,
-  upload,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
 
 export default videoRouter;
