14bb28e6b85a95e684fdcaa7d698ce2eeba9c280
himzei <himzei@gmail.com>
2021-04-16 10:52:19 +0900

    Template Pug Setting

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
-export const trending = (req, res) => res.send("Home page Videos");
-export const see = (req, res) => res.send("watch");
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => {
+  console.log(req.params);
+  res.send("watch");
+};
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
