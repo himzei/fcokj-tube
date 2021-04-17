1247ecb281307cb423c96b67ad1217734f1acc33
himzei <himzei@gmail.com>
2021-04-17 11:38:45 +0900

    Hashed Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,25 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
 };
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.end();
+export const postJoin = async (req, res) => {
+  const {
+    body: { name, username, email, password, location },
+  } = req;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  res.redirect("/login");
 };
+export const getLogin = (req, res) => res.send("Login");
+export const postLogin = (req, res) => {};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("logout");
 export const see = (req, res) => res.send("See");
