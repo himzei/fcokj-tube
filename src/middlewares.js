bf1dc00ca19312c2ee8535fd15d156004b5cb965
himzei <himzei@gmail.com>
2021-04-19 20:52:12 +0900

    Edit Profile

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,7 @@
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "외국인과동행";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.loggedInUser = req.session.user;
-  console.log(res.locals);
+  res.locals.loggedInUser = req.session.user || {};
+
   next();
 };
