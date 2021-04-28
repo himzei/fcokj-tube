15170e86fc90447edf44e02926f7958fdd679024
himzei <himzei@gmail.com>
2021-04-28 10:11:23 +0900

    21.6 Developer Experience

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,17 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/client/js/main.js",
+  mode: "development",
+  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
   ],
-  mode: "development",
   output: {
     filename: "js/main.js",
     path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
