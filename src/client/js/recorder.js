da0c47d7f5ae72de5b067e01d610e1a2410298d0
himzei <himzei@gmail.com>
2021-04-29 14:30:03 +0900

    25.4 Downloading the File

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,11 +3,20 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
-  startBtn.innerText = "Start Recording";
+  startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 
@@ -18,7 +27,7 @@ const handleStart = () => {
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     console.log(event.ondataavailable);
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
