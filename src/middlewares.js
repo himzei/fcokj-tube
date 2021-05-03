c864b9036cee8c76a221c59bb6756afdd907f48f
조현일 <himzei@gmail.com>
2021-05-03 18:04:25 +0900

    isHeroku

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,17 @@ const s3 = new aws.S3({
   },
 });
 
-const multerUploader = multerS3({
+const isHeroku = (process.env.NODE_ENV = "production");
+
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "fcokj-tube",
+  bucket: "fcokj-tube/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "fcokj-tube/videos",
   acl: "public-read",
 });
 
@@ -19,7 +27,7 @@ export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "외국인과동행";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user || {};
-
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -46,7 +54,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -54,5 +62,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: multerUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
